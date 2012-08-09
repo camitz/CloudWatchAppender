6fd5051de3e8a0fc9be2af3898e1e1745c55055a
Martin Camitz <martin.camitz@gmail.com>
2012-08-09 15:39:21 +0200

    Seed project

---
+++ RandomTicks/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using log4net;
+using log4net.Config;
+
+namespace RandomTicks
+{
+    internal class RandomTicks
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(RandomTicks));
+        
+        private static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+
+            var random = new Random();
+
+            while (true)
+            {
+                if (random.NextDouble() < .3)
+                    log.Info("A tick!");
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
+

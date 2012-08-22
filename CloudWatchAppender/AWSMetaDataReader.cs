5f460b709867554a80035af3cc11bd5f98ba8f60
Martin Camitz <martin.camitz@gmail.com>
2012-08-22 12:33:53 +0200

    Intance id is cached.
    Went to 1.1

--- CloudWatchAppender/AWSMetaDataReader.cs
+++ CloudWatchAppender/AWSMetaDataReader.cs
@@ -5,15 +5,20 @@ namespace CloudWatchAppender
 {
     static class AWSMetaDataReader
     {
+        private static string _readToEnd;
+
         public static string GetInstanceID()
         {
             try
             {
-                return new StreamReader(
-                    WebRequest.Create("http://169.254.169.254/latest/meta-data/instance-id")
-                        .GetResponse()
-                        .GetResponseStream(), true)
-                    .ReadToEnd();
+                if (string.IsNullOrEmpty(_readToEnd))
+                    _readToEnd = new StreamReader(
+                        WebRequest.Create("http://169.254.169.254/latest/meta-data/instance-id")
+                            .GetResponse()
+                            .GetResponseStream(), true)
+                        .ReadToEnd();
+
+                return _readToEnd;
             }
             catch (WebException)
             {

6565ab2fc7f8586def22a9482af63c9c17e9cc1c
Martin Camitz <martin.camitz@gmail.com>
2014-04-24 12:52:44 +0200

    Implemented UnitConverter.

---
+++ Tests/UnitConverterTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Amazon.CloudWatch;
+using CloudWatchAppender.Services;
+using NUnit.Framework;
+
+namespace CloudWatchAppender.Tests
+{
+    [TestFixture]
+    public class UnitConverterTests
+    {
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            Assert.That(new StandardUnit("Bytes"), Is.EqualTo(new StandardUnit("Bytes")));
+
+            Assert.That(UnitConverter.Convert(1000).From(StandardUnit.Kilobytes).To(StandardUnit.Bytes), Is.EqualTo(1024000.0));
+            Assert.That(UnitConverter.Convert(1000).From(StandardUnit.Megabytes).To(StandardUnit.Bytes), Is.EqualTo(1048576000.0));
+            Assert.That(() => { UnitConverter.Convert(1000).From(StandardUnit.Megabytes).To(StandardUnit.Seconds); }, Throws.Exception);
+
+            Assert.That(UnitConverter.Convert(1000).From(StandardUnit.Terabytes).To(StandardUnit.Terabits), Is.EqualTo(8000));
+            Assert.That(UnitConverter.Convert(1000).From(StandardUnit.TerabytesSecond).To(StandardUnit.TerabitsSecond), Is.EqualTo(8000));
+        }
+    }
+}

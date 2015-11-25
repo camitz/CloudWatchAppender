20539c10b0d858eafa92b65a8780a32f613bebe7
Joe Rall <Joseph.Rall@Live.com>
2015-11-25 13:48:22 -0500

    Moving init into install so it can actually be run.

--- BuildScripts/install.ps1
+++ BuildScripts/install.ps1
@@ -1,3 +1,18 @@
+$parsedReleaseBuildVersion = $env:APPVEYOR_REPO_TAG_NAME -Match "(\d+.\d+.\d+(.\d+)?)"
+    
+If($env:appveyor_repo_tag -AND $parsedReleaseBuildVersion) {
+	$env:BuildVersion = $matches[0]
+	$env:IsGithubRelease = $TRUE
+}
+else {
+	$env:BuildVersion = $env:appveyor_build_version
+	$env:IsGithubRelease = ""
+}
+
+Write-Host "Build Version: " $env:BuildVersion
+
+Write-Host "appveyor_build_version Variable: " $env:appveyor_build_version
+
 try
 {
 	if($env:IsGithubRelease)

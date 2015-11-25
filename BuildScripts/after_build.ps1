b0aa256967fc990470cb68df03cb228a269ebb1c
Joe Rall <Joseph.Rall@Live.com>
2015-11-25 13:11:00 -0500

    Refactored build script into it's own files. Added artifact for build

---
+++ BuildScripts/after_build.ps1
@@ -0,0 +1,13 @@
+#Create Folder Structure
+New-Item -Force -ItemType directory -Path build\lib\net35
+New-Item -Force -ItemType directory -Path build\lib\net45
+
+#Copy Files into folders
+Copy-Item -Force "CloudWatchAppender3.5\bin\Release\CloudWatchAppender.dll" "build\lib\net35"
+Copy-Item -Force "CloudWatchAppender3.5\bin\Release\CloudWatchAppender.pdb" "build\lib\net35"
+Copy-Item -Force "CloudWatchAppender\bin\Release\CloudWatchAppender.pdb" "build\lib\net45"
+Copy-Item -Force "CloudWatchAppender\bin\Release\CloudWatchAppender.dll" "build\lib\net45"
+
+$nuspecPath = (Get-ChildItem -Filter *.nuspec -Recurse )[0].FullName
+
+nuget pack $nuspecPath

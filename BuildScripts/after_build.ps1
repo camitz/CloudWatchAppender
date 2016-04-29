d2e25394be0612ceb601cb67ad8ee4ba276f4683
Martin Camitz <martin.camitz@gmail.com>
2016-04-29 11:34:21 +0200

    After build script to create all sub projects nugets.

--- BuildScripts/after_build.ps1
+++ BuildScripts/after_build.ps1
@@ -2,14 +2,107 @@
 New-Item -Force -ItemType directory -Path build\lib\net35
 New-Item -Force -ItemType directory -Path build\lib\net45
 
-#Copy Files into folders
-Copy-Item -Force "CloudWatchAppender3.5\bin\Release\CloudWatchAppender.dll" "build\lib\net35"
-Copy-Item -Force "CloudWatchAppender3.5\bin\Release\CloudWatchAppender.pdb" "build\lib\net35"
-Copy-Item -Force "CloudWatchAppender\bin\Release\CloudWatchAppender.pdb" "build\lib\net45"
-Copy-Item -Force "CloudWatchAppender\bin\Release\CloudWatchAppender.dll" "build\lib\net45"
 
-$nuspecPath = (Get-ChildItem -Filter *.nuspec -Recurse )[0].FullName
+#Copy Files into folders
+Copy-Item -Force "Appenders\CloudWatchAppender\bin\Release\AWSAppender.CloudWatch.dll" "build\lib\net45"
+Copy-Item -Force "Appenders\CloudWatchAppender\bin\Release\AWSAppender.CloudWatch.pdb" "build\lib\net45"
+Copy-Item -Force "Appenders\CloudWatchLogsAppender\bin\Release\AWSAppender.CloudWatchLogs.dll" "build\lib\net45"
+Copy-Item -Force "Appenders\CloudWatchLogsAppender\bin\Release\AWSAppender.CloudWatchLogs.pdb" "build\lib\net45"
+Copy-Item -Force "AWSAppender.Core\bin\Release\AWSAppender.Core.dll" "build\lib\net45"
+Copy-Item -Force "AWSAppender.Core\bin\Release\AWSAppender.Core.pdb" "build\lib\net45"
+
+Copy-Item -Force "Appenders\CloudWatchAppender3.5\bin\Release\AWSAppender.CloudWatch.dll" "build\lib\net35"
+Copy-Item -Force "Appenders\CloudWatchAppender3.5\bin\Release\AWSAppender.CloudWatch.pdb" "build\lib\net35"
+Copy-Item -Force "Appenders\CloudWatchLogsAppender3.5\bin\Release\AWSAppender.CloudWatchLogs.dll" "build\lib\net35"
+Copy-Item -Force "Appenders\CloudWatchLogsAppender3.5\bin\Release\AWSAppender.CloudWatchLogs.pdb" "build\lib\net35"
+Copy-Item -Force "AWSAppender.Core3.5\bin\Release\AWSAppender.Core.dll" "build\lib\net35"
+Copy-Item -Force "AWSAppender.Core3.5\bin\Release\AWSAppender.Core.pdb" "build\lib\net35"
+
+
+$nuspecPath = (Get-ChildItem -Filter CloudWatchAppender.nuspec -Recurse ).FullName
 
 nuget pack $nuspecPath
 
 Remove-Item -Force build\lib\net35
+Remove-Item -Force build\lib\net45
+
+
+#Copy Files into folders
+Copy-Item -Force "AWSAppender.Core\bin\Release\AWSAppender.Core.dll" "build\lib\net45"
+Copy-Item -Force "AWSAppender.Core\bin\Release\AWSAppender.Core.pdb" "build\lib\net45"
+
+Copy-Item -Force "AWSAppender.Core3.5\bin\Release\AWSAppender.Core.dll" "build\lib\net35"
+Copy-Item -Force "AWSAppender.Core3.5\bin\Release\AWSAppender.Core.pdb" "build\lib\net35"
+
+
+$nuspecPath = (Get-ChildItem -Filter AWSAppender.Core.nuspec -Recurse ).FullName
+
+nuget pack $nuspecPath
+
+Remove-Item -Force build\lib\net35
+Remove-Item -Force build\lib\net45
+
+
+#Copy Files into folders
+Copy-Item -Force "Appenders\CloudWatchAppender\bin\Release\AWSAppender.CloudWatch.dll" "build\lib\net45"
+Copy-Item -Force "Appenders\CloudWatchAppender\bin\Release\AWSAppender.CloudWatch.pdb" "build\lib\net45"
+
+Copy-Item -Force "Appenders\CloudWatchAppender3.5\bin\Release\AWSAppender.CloudWatch.dll" "build\lib\net35"
+Copy-Item -Force "Appenders\CloudWatchAppender3.5\bin\Release\AWSAppender.CloudWatch.pdb" "build\lib\net35"
+
+
+$nuspecPath = (Get-ChildItem -Filter AWSAppender.CloudWatch.nuspec -Recurse ).FullName
+
+nuget pack $nuspecPath
+
+Remove-Item -Force build\lib\net35
+Remove-Item -Force build\lib\net45
+
+
+#Copy Files into folders
+Copy-Item -Force "Appenders\CloudWatchLogsAppender\bin\Release\AWSAppender.CloudWatchLogs.dll" "build\lib\net45"
+Copy-Item -Force "Appenders\CloudWatchLogsAppender\bin\Release\AWSAppender.CloudWatchLogs.pdb" "build\lib\net45"
+
+Copy-Item -Force "Appenders\CloudWatchLogsAppender3.5\bin\Release\AWSAppender.CloudWatchLogs.dll" "build\lib\net35"
+Copy-Item -Force "Appenders\CloudWatchLogsAppender3.5\bin\Release\AWSAppender.CloudWatchLogs.pdb" "build\lib\net35"
+
+
+$nuspecPath = (Get-ChildItem -Filter AWSAppender.CloudWatchLogs.nuspec -Recurse ).FullName
+
+nuget pack $nuspecPath
+
+Remove-Item -Force build\lib\net35
+Remove-Item -Force build\lib\net45
+
+#Copy Files into folders
+Copy-Item -Force "Appenders\SQSAppender\bin\Release\AWSAppender.SQS.dll" "build\lib\net45"
+Copy-Item -Force "Appenders\SQSAppender\bin\Release\AWSAppender.SQS.pdb" "build\lib\net45"
+
+Copy-Item -Force "Appenders\SQSAppender3.5\bin\Release\AWSAppender.SQS.dll" "build\lib\net35"
+Copy-Item -Force "Appenders\SQSAppender3.5\bin\Release\AWSAppender.SQS.pdb" "build\lib\net35"
+
+
+$nuspecPath = (Get-ChildItem -Filter AWSAppender.SQS.nuspec -Recurse ).FullName
+
+nuget pack $nuspecPath
+
+Remove-Item -Force build\lib\net35
+Remove-Item -Force build\lib\net45
+
+
+
+#Copy Files into folders
+Copy-Item -Force "Appenders\SNSAppender\bin\Release\AWSAppender.SNS.dll" "build\lib\net45"
+Copy-Item -Force "Appenders\SNSAppender\bin\Release\AWSAppender.SNS.pdb" "build\lib\net45"
+
+Copy-Item -Force "Appenders\SNSAppender3.5\bin\Release\AWSAppender.SNS.dll" "build\lib\net35"
+Copy-Item -Force "Appenders\SNSAppender3.5\bin\Release\AWSAppender.SNS.pdb" "build\lib\net35"
+
+
+$nuspecPath = (Get-ChildItem -Filter AWSAppender.SNS.nuspec -Recurse ).FullName
+
+nuget pack $nuspecPath
+
+Remove-Item -Force build\lib\net35
+Remove-Item -Force build\lib\net45
+
